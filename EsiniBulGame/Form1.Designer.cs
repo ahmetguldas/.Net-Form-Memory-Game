09531b9c02b750cfe29cb60e597c380d2f68400b
ahmet güldaş <ahmet.guldas@hotmail.com>
2021-06-04 21:08:27 +0300

    Add project files.

---
+++ EsiniBulGame/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace EsiniBulGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlKartlar = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pnlKartlar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlKartlar
+            // 
+            this.pnlKartlar.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.pnlKartlar.Location = new System.Drawing.Point(21, 21);
+            this.pnlKartlar.Margin = new System.Windows.Forms.Padding(12);
+            this.pnlKartlar.Name = "pnlKartlar";
+            this.pnlKartlar.Size = new System.Drawing.Size(1000, 1000);
+            this.pnlKartlar.TabIndex = 0;
+            this.pnlKartlar.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1042, 1043);
+            this.Controls.Add(this.pnlKartlar);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Esini Bul";
+            ((System.ComponentModel.ISupportInitialize)(this.pnlKartlar)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pnlKartlar;
+    }
+}
+
---
+++ EsiniBulGame/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EsiniBulGame
+{
+    public partial class Form1 : Form
+    {
+        int satirAdet = 4;
+        int sutunAdet = 4;
+        int kartBoyut = 75;
+        int kartArasi = 5;
+        public int KartAdet => satirAdet * sutunAdet;
+        public Form1()
+        {
+            InitializeComponent();
+            KartlariDiz();
+        }
+
+        private void KartlariDiz()
+        {
+            pnlKartlar.Width = sutunAdet * (kartBoyut + kartArasi) - kartArasi;
+            pnlKartlar.Height = satirAdet * (kartBoyut + kartArasi) - kartArasi;
+            ClientSize = new Size(pnlKartlar.Width + 20, pnlKartlar.Height + 20);
+
+            for (int i = 0; i < KartAdet; i++)
+            {
+                PictureBox pb = new PictureBox();
+                pb.Width = kartBoyut;
+                pb.Height = kartBoyut;
+                pb.BackColor = Color.Red;
+                pb.Left = i % sutunAdet * (kartBoyut + kartArasi);
+                pb.Top = i / sutunAdet * (kartBoyut + kartArasi);
+                pnlKartlar.Controls.Add(pb);
+            }
+        }
+    }
+}
